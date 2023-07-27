5f47eddd302a95687e3d3b15574878b47a629ea2
Cameron <cameronghm@gmail.com>
2023-07-27 12:15:37 +0100

    Updated QLC-2

--- java-api/src/main/java/com/db/grad/javaapi/service/DogHandler.java
+++ java-api/src/main/java/com/db/grad/javaapi/service/DogHandler.java
@@ -3,6 +3,8 @@ package com.db.grad.javaapi.service;
 import com.db.grad.javaapi.repository.DogsRepository;
 import com.db.grad.javaapi.model.*;
 
+import java.util.List;
+
 public class DogHandler {
     private DogsRepository itsDogRepo;
     public DogHandler(DogsRepository itsDogRepo) {
@@ -15,5 +17,36 @@ public class DogHandler {
 
     public long getNoOfDogs() {
         return itsDogRepo.count();
+    }
+
+    public Dog getDogByName(String name)
+    {
+        Dog sampleDog = new Dog();
+        sampleDog.setName(name);
+        List<Dog> results = itsDogRepo.findByName(sampleDog);
+        if (results.size()==1)
+        {
+            return results.get(0);
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    public Dog getDogByid(long theDogid) {
+        return itsDogRepo.findById(theDogid);
+    }
+
+    public long updateDogDetails(Dog theDog) {
+        System.out.println(theDog.getId());
+        System.out.println(itsDogRepo.existsById(1));
+        System.out.println(itsDogRepo.existsById(2));
+        if (!itsDogRepo.existsById(theDog.getId())){
+            return -1;
+        }
+        else {
+            return itsDogRepo.save(theDog);
+        }
     }
 }
