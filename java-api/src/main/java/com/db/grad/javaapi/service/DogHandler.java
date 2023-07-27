50b41a3394ad41bd9e42b993c906c9337107cb6d
AlexandraCimpoia <alexandra.cimpoia13@gmail.com>
2023-07-27 14:23:50 +0100

    Added tests

--- java-api/src/main/java/com/db/grad/javaapi/service/DogHandler.java
+++ java-api/src/main/java/com/db/grad/javaapi/service/DogHandler.java
@@ -3,9 +3,12 @@ package com.db.grad.javaapi.service;
 import com.db.grad.javaapi.model.Dog;
 import com.db.grad.javaapi.repository.DogsRepository;
 
+import java.util.List;
+
 public class DogHandler {
 
     private DogsRepository itsDogsRepo;
+
     public DogHandler(DogsRepository repo) {
         itsDogsRepo = repo;
     }
@@ -16,5 +19,40 @@ public class DogHandler {
 
     public long getNoOfDogs() {
         return itsDogsRepo.count();
+    }
+
+    public Dog getDogByName(String name) {
+        Dog dog = new Dog();
+        dog.setName(name);
+        List<Dog> dogs = itsDogsRepo.findByName(dog);
+
+        if (dogs.size() == 1) {
+            return dogs.get(0);
+        } else {
+            return null;
+        }
+    }
+
+    public Dog getDogById(long dogId) {
+        return itsDogsRepo.findById(dogId);
+    }
+
+    public boolean removeDog(long uniqueId) {
+        boolean result = false;
+
+        Dog theDog = itsDogsRepo.findById(uniqueId);
+        if (theDog != null) {
+            result = itsDogsRepo.delete(theDog);
+        }
+
+        return result;
+    }
+
+    public long updateDogDetails(Dog theDog) {
+        if (!itsDogsRepo.existsById(theDog.getId())) {
+            return -1;
+        } else {
+            return itsDogsRepo.save(theDog);
+        }
     }
 }
--- java-api/src/test/java/com/db/grad/javaapi/service/DogsHandlerTest.java
+++ java-api/src/test/java/com/db/grad/javaapi/service/DogsHandlerTest.java
@@ -7,15 +7,18 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 public class DogsHandlerTest {
     private DogsRepository itsDogRepo = new DogsRepositoryStub();
+
     @BeforeEach
-    public void makeSureRepoIsEmpty(){
+    public void makeSureRepoIsEmpty() {
         itsDogRepo.deleteAll();
     }
+
     @Test
-    public void add_a_dog_return_number_of_dogs_in_repo_is_one () {
+    public void add_a_dog_return_number_of_dogs_in_repo_is_one() {
         //arrange
         DogHandler cut = new DogHandler(itsDogRepo);
         Dog theDog = new Dog();
@@ -29,6 +32,141 @@ public class DogsHandlerTest {
 
         //assert
         assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void add_several_dogs_return_number_of_dogs_match_number_added() {
+        // arrange
+        DogHandler cut = new DogHandler(itsDogRepo);
+
+        Dog dog1 = new Dog();
+        dog1.setName("Dog1");
+        cut.addDog(dog1);
+        Dog dog2 = new Dog();
+        dog2.setName("Dog2");
+        cut.addDog(dog2);
+        Dog dog3 = new Dog();
+        dog3.setName("Dog3");
+        cut.addDog(dog3);
+
+        int expectedResult = 3;
+
+        // act
+        long actualResult = cut.getNoOfDogs();
+
+        // assert
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void get_dog_by_name_null_if_no_dogs() {
+        // arrange
+        DogHandler cut = new DogHandler(itsDogRepo);
+
+        // act
+        Dog actualResult = cut.getDogByName("Dog");
+        Dog expectedResult = null;
+
+        // assert
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void find_dog_by_name_returns_null_when_many_dogs_with_same_name() {
+        // arrange
+        DogHandler cut = new DogHandler(itsDogRepo);
+
+        Dog dog1 = new Dog();
+        dog1.setName("Dog1");
+        cut.addDog(dog1);
+        Dog dog2 = new Dog();
+        dog2.setName("Dog1");
+        cut.addDog(dog2);
+        Dog dog3 = new Dog();
+        dog3.setName("Dog3");
+        cut.addDog(dog3);
+
+        Dog expectedDog = dog1;
+        String dogToFind = "Dog1";
+
+        // act
+        Dog actualResult = cut.getDogByName(dogToFind);
+
+        // assert
+        assertNull(actualResult);
+    }
+
+    @Test
+    public void find_dog_by_valid_id_returns_one_dog() {
+        // arrange
+        DogHandler cut = new DogHandler(itsDogRepo);
+
+        Dog dog1 = new Dog();
+        dog1.setName("Dog1");
+        cut.addDog(dog1);
+        Dog dog2 = new Dog();
+        dog2.setName("Dog2");
+        cut.addDog(dog2);
+        Dog dog3 = new Dog();
+        dog3.setName("Dog3");
+        cut.addDog(dog3);
+        long dogId = cut.addDog(dog1);
+        Dog expectedDog = dog1;
+
+        // act
+        Dog actualResult = cut.getDogById(dogId);
+
+        // assert
+        assertEquals(expectedDog.getId(), actualResult.getId());
+        assertEquals(expectedDog.getName(), actualResult.getName());
+
+    }
+
+    @Test
+    public void add_dog_and_remove_dog_return_number_of_dogs_is_zero() {
+        // arrange
+        DogHandler cut = new DogHandler(itsDogRepo);
+        Dog theDog = new Dog();
+        theDog.setName("Bruno");
+        long dogId = cut.addDog(theDog);
+
+        long expectedResult = 0;
+        boolean expectedStatus = true;
+
+
+        // act
+        boolean actualStatus = cut.removeDog(dogId);
+        long actualResult = cut.getNoOfDogs();
+
+        // assert
+        assertEquals(expectedStatus, actualStatus);
+        assertEquals(expectedResult, actualResult);
+
+    }
+
+    @Test
+    public void update_dog_that_exists_returns_dog_id() {
+        // arrange
+        DogHandler cut = new DogHandler(itsDogRepo);
+        Dog theDog = new Dog();
+        theDog.setName("Bruno");
+        cut.addDog(theDog);
+        theDog = new Dog();
+        theDog.setName("Frank");
+        long expectedResult = cut.addDog(theDog);
+        Dog dogToUpdate = theDog;
+        theDog = new Dog();
+        theDog.setName("Penny");
+        cut.addDog(theDog);
+
+        // act
+        dogToUpdate.setName("Charlie");
+        long actualResult = cut.updateDogDetails(dogToUpdate);
+
+        // assert
+        assertEquals(expectedResult, actualResult);
 
     }
 }
+
+
