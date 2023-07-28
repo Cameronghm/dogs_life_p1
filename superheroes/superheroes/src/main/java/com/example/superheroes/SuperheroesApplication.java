6ccef97b1ff17e8100761ab9b040ce69ab02f933
bunarty <bashirsolomon321@gmail.com>
2023-07-28 15:01:12 +0100

    QL4 Use of Jpa Repository : Task - Create the Hero model, Create and use the hero repository.

--- superheroes/superheroes/src/main/java/com/example/superheroes/SuperheroesApplication.java
+++ superheroes/superheroes/src/main/java/com/example/superheroes/SuperheroesApplication.java
@@ -1,17 +1,26 @@
 package com.example.superheroes;
 
+
+import com.example.superheroes.model.Hero;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.example.superheroes.practice.Calculator;
+import com.example.superheroes.repository.HeroRepository;
+
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
 
 @SpringBootApplication
 public class SuperheroesApplication implements CommandLineRunner {
 
 	@Autowired
 	Calculator calc;
+
+	@Autowired
+	HeroRepository heroRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SuperheroesApplication.class, args);
@@ -20,7 +29,22 @@ public class SuperheroesApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		System.out.println("Hello World");
-		int result = calc.addition(3,4);
+		int result = calc.addition(3, 4);
 		System.out.println(result);
+
+		List<Hero> heroes = heroRepository.findAll();
+		List<Hero> heroes1 = heroRepository.findTop3ByOrderByNameDesc();
+		List<Hero> heroes2 = heroRepository.findHeroesNameStartingWithLetter("C%");
+		for (Hero hero : heroes) {
+			System.out.println(hero);
+		}
+		System.out.println(" ");
+		for (Hero hero : heroes1) {
+			System.out.println(hero);
+		}
+		System.out.println(" ");
+		for (Hero hero : heroes2) {
+			System.out.println(hero);
+		}
 	}
 }
