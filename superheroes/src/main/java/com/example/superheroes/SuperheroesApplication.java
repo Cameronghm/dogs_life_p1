7d06202a50af634a71e79425a78ec5f5818db41b
AlexandraCimpoia <alexandra.cimpoia13@gmail.com>
2023-07-28 12:55:26 +0100

    Added SpringBoot QL5

--- superheroes/src/main/java/com/example/superheroes/SuperheroesApplication.java
+++ superheroes/src/main/java/com/example/superheroes/SuperheroesApplication.java
@@ -3,6 +3,7 @@ package com.example.superheroes;
 import com.example.superheroes.model.Hero;
 import com.example.superheroes.practice.Calculator;
 import com.example.superheroes.repository.HeroRepository;
+import com.example.superheroes.service.HeroService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -15,8 +16,11 @@ public class SuperheroesApplication implements CommandLineRunner {
 
     //@Autowired
     //Calculator calc;
+//    @Autowired
+//    HeroRepository heroRepository;
+
     @Autowired
-    HeroRepository heroRepository;
+    HeroService heroService;
 
     public static void main(String[] args) {
         SpringApplication.run(SuperheroesApplication.class, args);
@@ -30,8 +34,27 @@ public class SuperheroesApplication implements CommandLineRunner {
 
         //List<Hero> heroes = heroRepository.findAll();
         //List<Hero> heroes = heroRepository.findTop3ByOrderByNameDesc();
-        List<Hero> heroes = heroRepository.findHeroesNameStartingWithLetter("C%");
-        for (Hero hero: heroes){
+//        List<Hero> heroes = heroRepository.findHeroesNameStartingWithLetter("C%");
+//        for (Hero hero: heroes){
+//            System.out.println(hero);
+//        }
+
+        List<Hero> heroes = heroService.getHeroesStartingWithLetter("C");
+        for (Hero hero : heroes){
+            System.out.println(hero);
+        }
+
+        System.out.println("___________________________________________");
+
+        List<Hero> allHeroes = heroService.getAllHeroes();
+        for (Hero hero : allHeroes){
+            System.out.println(hero);
+        }
+
+        System.out.println("___________________________________________");
+
+        List<Hero> lastThreeHeroes = heroService.getLastThreeHeroesByName();
+        for (Hero hero : lastThreeHeroes){
             System.out.println(hero);
         }
     }
