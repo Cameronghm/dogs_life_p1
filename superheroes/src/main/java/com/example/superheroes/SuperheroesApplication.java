d8195b6961eb9655ff1842bfaf10c741fe5fe3d4
Jannik Kuom <j.kuom@t-online.de>
2023-07-28 14:03:16 +0100

    QL5 done

--- superheroes/src/main/java/com/example/superheroes/SuperheroesApplication.java
+++ superheroes/src/main/java/com/example/superheroes/SuperheroesApplication.java
@@ -1,7 +1,7 @@
 package com.example.superheroes;
 
 import com.example.superheroes.model.Hero;
-import com.example.superheroes.repository.HeroRepository;
+import com.example.superheroes.service.HeroService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -12,7 +12,7 @@ import java.util.List;
 @SpringBootApplication public class SuperheroesApplication implements CommandLineRunner {
 
 	@Autowired
-	HeroRepository heroRepository;
+	HeroService heroService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SuperheroesApplication.class, args);
@@ -20,8 +20,22 @@ import java.util.List;
 
 	@Override
 	public void run(String... args) throws Exception {
-		List<Hero> heroes = heroRepository.findHeroesNameStartingWithLetter("C%");
-		for (Hero hero: heroes){
+		List<Hero> heroes = heroService.getHeroesStartingWithLetter("C");
+		for (Hero hero : heroes){
+			System.out.println(hero);
+		}
+
+		System.out.println("___________________________________________");
+
+		List<Hero> allHeroes = heroService.getAllHeroes();
+		for (Hero hero : allHeroes){
+			System.out.println(hero);
+		}
+
+		System.out.println("___________________________________________");
+
+		List<Hero> lastThreeHeroes = heroService.getLastThreeHeroesByName();
+		for (Hero hero : lastThreeHeroes){
 			System.out.println(hero);
 		}
 	}
