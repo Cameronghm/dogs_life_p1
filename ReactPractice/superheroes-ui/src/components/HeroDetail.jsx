44ee1d0a691eed04ac8cd6385c1fb720a91a6377
bunarty <bashirsolomon321@gmail.com>
2023-07-31 09:23:51 +0100

    QL5 - Add State - Count the likes

--- ReactPractice/superheroes-ui/src/components/HeroDetail.jsx
+++ ReactPractice/superheroes-ui/src/components/HeroDetail.jsx
@@ -2,8 +2,14 @@ import React from 'react';
 import images from '../index';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import { useState } from 'react';
+import { Row, Col } from 'react-bootstrap';
 
 const HeroDetail = (props) => {
+  const[counter, setCounter] = useState(0);
+  const incrementCounter =()=>{
+    setCounter(counter+1);
+  }
   return (
     <Card>
     <div className="imgContainer">
@@ -12,7 +18,10 @@ const HeroDetail = (props) => {
     <Card.Body>
       <Card.Title>Alias: {props.info.alias}</Card.Title>
       <Card.Text>Name: {props.info.name}</Card.Text>
-      <Button variant="primary">Like</Button>
+      <Row>
+        <Col><Card.Text>Likes: {counter}</Card.Text></Col>
+        <Col><Button variant="primary" onClick={incrementCounter}>Like</Button></Col>
+      </Row>
     </Card.Body>
   </Card>
   )
