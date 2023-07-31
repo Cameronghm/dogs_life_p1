3a24df6358aa5c3fa2e349f88c12223f5a2127d0
bunarty <bashirsolomon321@gmail.com>
2023-07-31 09:51:34 +0100

    QL6 - Add inverse data flow - prepare parent component & use the props in child

--- ReactPractice/superheroes-ui/src/components/AllHeroes.jsx
+++ ReactPractice/superheroes-ui/src/components/AllHeroes.jsx
@@ -2,10 +2,18 @@ import React from 'react'
 import HeroDetail from './HeroDetail'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
+import { useState } from 'react'
 
 const AllHeroes = () => {
 
-    const heroes =[{"id":1,"name":"Bruce Wayne","alias":"Batman","superpower":"Martial Arts","teamid":1},
+  const [total, setTotal] = useState(0);
+
+  const totalCountHandler = (name) =>{
+    console.log(name)
+    setTotal(total+1);
+}
+
+  const heroes =[{"id":1,"name":"Bruce Wayne","alias":"Batman","superpower":"Martial Arts","teamid":1},
     {"id":2,"name":"Clark Kent","alias":"Superman","superpower":"Flight","teamid":1},
     {"id":3,"name":"Jay Garrick","alias":"The Flash","superpower":"Super-speed","teamid":2},
     {"id":4,"name":"Alan Scott","alias":"Green Lantern","superpower":"Ring Creation","teamid":2},
@@ -17,18 +25,21 @@ const AllHeroes = () => {
     {"id":10,"name":"Dick Grayson","alias":"Nightwing","superpower":"Acrobatics","teamid":5},
     ]
   return (
-    <Row>
-      {
-      heroes.map(hero=>(
-        <Col> {
-          <div className='container' key={hero.id.toString()}>
-            <HeroDetail info={hero}/>
-            </div>
-          }
-        </Col>
-        ))
-      }
-    </Row>
+    <>
+      <h1> Total count is: {total}</h1>  
+      <Row>
+        {
+        heroes.map(hero=>(
+          <Col> {
+            <div className='container' key={hero.id.toString()}>
+              <HeroDetail info={hero}  parentCount={totalCountHandler} />
+              </div>
+            }
+          </Col>
+          ))
+        }
+      </Row>
+    </>  
   )
 }
 
