dc9905567873882fcaa2c4d15dbdd1454137e348
Jannik Kuom <j.kuom@t-online.de>
2023-07-31 14:30:48 +0100

    React QL2 Task 2

--- react_practice/superheroes-ui/src/components/HeroDetail.jsx
+++ react_practice/superheroes-ui/src/components/HeroDetail.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import batman from '../images/download.png';
 
 const HeroDetail = () => {
-  return (
-    <div>HeroDetail</div>
-  )
+      return (
+        <div>
+            <img src={batman} alt="Batman" height="250px"/>
+            <p id="blue-alias">Alias: Batman</p>
+            <p className="green-class">Name: Bruce Wayne</p>
+            <button className="green-class">Like</button>   
+        </div>
+    )
 }
-
 export default HeroDetail
