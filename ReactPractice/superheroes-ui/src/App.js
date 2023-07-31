15811a6c1f5ac15d5627e2b429d0577e1c75582b
Cameron Mitchell <cameronghm@gmail.com>
2023-07-31 15:47:27 +0100

    QL-3

--- ReactPractice/superheroes-ui/src/App.js
+++ ReactPractice/superheroes-ui/src/App.js
@@ -1,6 +1,6 @@
-import logo from './logo.svg';
 import './App.css';
 import AllHeroes from './components/AllHeroes';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
--- ReactPractice/superheroes-ui/src/components/HeroDetail.jsx
+++ ReactPractice/superheroes-ui/src/components/HeroDetail.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
-import batman from '../images/imageofbatman.jpeg'
+import images from '../index'
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
 const HeroDetail = (props) => {
   return (
-    <div>
-        <img src={batman} alt="Batman" height="250px" />
-        <p id="blue-alias">Alias: {props.info.alias}</p>
-        <p class="green-class">Name: {props.info.name}</p>
-        <button class="green-class">Like</button>
-    </div>
+    <Card >
+      <div className="imgContainer">
+        <Card.Img variant="top" src={images[props.info.alias.replaceAll(/\s/g,'')]} width="150px" />
+      </div>
+      <Card.Body >
+        <Card.Title >Alias: {props.info.alias}</Card.Title>
+        <Card.Text >Name: {props.info.name}</Card.Text>
+        <Button variant="primary">Like</Button>
+      </Card.Body>
+    </Card>
   )
 }
 
