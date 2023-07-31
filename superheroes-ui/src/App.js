f4a6c65d48d5302e9572929b344dbf6d25907725
AlexandraCimpoia <alexandra.cimpoia13@gmail.com>
2023-07-31 16:42:51 +0100

    added React-practice

---
+++ superheroes-ui/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import AllHeroes from './components/AllHeroes';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+   <AllHeroes/>
+  );
+}
+
+export default App;
