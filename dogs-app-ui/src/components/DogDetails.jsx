3c7ee7d997114edfcba346a8b34bb3a7eb4f8b91
AlexandraCimpoia <alexandra.cimpoia13@gmail.com>
2023-08-01 16:02:11 +0100

    Added 1. c.

--- dogs-app-ui/src/components/DogDetails.jsx
+++ dogs-app-ui/src/components/DogDetails.jsx
@@ -1,20 +1,38 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import { useEffect, useState } from 'react';
 
 const DogDetail = (props) => {
+  const [ownersId, setOwnersId] = useState(false);
+  
+
+  const makeOwnersIdVisible = () => {
+    setOwnersId(current => !current);
+  }
+
+  useEffect(() => {
+    console.log(ownersId);
+  }, [ownersId]);
+
   return (
     <Card>
-        <Card.Body>
+      <Card.Body>
         <Card.Title>Dog's Name: {props.info.name}</Card.Title>
         <Card.Text>Dog's Age: {props.info.age}</Card.Text>
         <Card.Text>Dog's ID: {props.info.id}</Card.Text>
-        </Card.Body>
+        <Button variant="primary" onClick={makeOwnersIdVisible}>Show Owner's Id</Button>
+        {ownersId &&
+        <Card.Text>Owner's ID: {props.info.owner_id}</Card.Text>
+         
+        }
+      </Card.Body>
     </Card>
   )
 
 }
 export default DogDetail
 
-  
+
 
 
--- dogs-app-ui/src/components/DogForm.jsx
+++ dogs-app-ui/src/components/DogForm.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const DogForm = () => {
   return (
-    <div>DogForm</div>
+    <div></div>
   )
 }
 
